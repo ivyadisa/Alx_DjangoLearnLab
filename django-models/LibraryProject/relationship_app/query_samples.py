bcdf78db17753cc0af317fb4735ff108da4346ce
Ivy <ivyadisa7@gmail.com>
2025-09-05 23:33:22 +0300

    Add relationship_app with advanced model relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,26 +3,15 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
 def books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        return Book.objects.filter(author=author)
-    except Author.DoesNotExist:
-        return []
+    return Book.objects.filter(author__name=author_name)
 
 
 # 2. List all books in a library
 def books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.books.all()
-    except Library.DoesNotExist:
-        return []
+    return Library.objects.get(name=library_name).books.all()
 
 
 # 3. Retrieve the librarian for a library
 def librarian_of_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        return library.librarian
-    except (Library.DoesNotExist, Librarian.DoesNotExist):
-        return None
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
