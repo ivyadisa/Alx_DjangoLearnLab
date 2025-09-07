e3926bcca41d8c595a5f73d44858d754b9c71527
Ivy <ivyadisa7@gmail.com>
2025-09-07 14:46:38 +0300

    views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView 
 from .models import Book
 from .models import Library   # <-- keep this line exactly like this
 
@@ -7,9 +7,8 @@ from .models import Library   # <-- keep this line exactly like this
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
-
-# Class-based view
+#class function
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+    context_object_name = "library
