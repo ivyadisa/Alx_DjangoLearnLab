32558172508ec320aa1e99a6b91b9e89d8cd1036
Ivy <ivyadisa7@gmail.com>
2025-09-07 16:06:36 +0300

    url changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('member-view/', member_view, name='member_view'),
 
     # Book CRUD views
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
