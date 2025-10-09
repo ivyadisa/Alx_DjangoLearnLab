5bf22adf5217703dc3847cb7e0ac4897462337ca
ivyadisa <ivyadisa7@gmail.com>
2025-10-09 10:26:59 +0300

    posts

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,60 +1,32 @@
-from rest_framework import viewsets, permissions, status, filters
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
-from rest_framework.pagination import PageNumberPagination
-from django_filters.rest_framework import DjangoFilterBackend
 
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 100
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions for GET, HEAD, OPTIONS requests
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Only allow the owner to modify
+        return obj.author == request.user
+
 
 class PostViewSet(viewsets.ModelViewSet):
-    """
-    CRUD for Posts.
-    - create: authenticated users
-    - update/destroy: only author
-    - list/retrieve: public (IsAuthenticatedOrReadOnly)
-    """
-    queryset = Post.objects.select_related('author').prefetch_related('comments').all()
+    queryset = Post.objects.all()  
     serializer_class = PostSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    pagination_class = StandardResultsSetPagination
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['author__username']
-    search_fields = ['title', 'content']
-    ordering_fields = ['created_at', 'updated_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    # example custom action: latest comments
-    @action(detail=True, methods=['get'])
-    def recent_comments(self, request, pk=None):
-        post = self.get_object()
-        qs = post.comments.order_by('-created_at')[:10]
-        serializer = CommentSerializer(qs, many=True, context={'request': request})
-        return Response(serializer.data)
 
 class CommentViewSet(viewsets.ModelViewSet):
-    """
-    CRUD for Comments.
-    - create: authenticated users (post must exist)
-    - update/destroy: only author
-    - list: optionally filter by post (use ?post=<id>)
-    """
-    queryset = Comment.objects.select_related('author', 'post').all()
+    queryset = Comment.objects.all() 
     serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    pagination_class = StandardResultsSetPagination
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['post__id', 'author__username']
-    search_fields = ['content']
-    ordering_fields = ['created_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
