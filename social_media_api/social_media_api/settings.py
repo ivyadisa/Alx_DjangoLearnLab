07fcfa951a381fd286f5ea89c05eaf2eed8f6225
ivyadisa <ivyadisa7@gmail.com>
2025-10-10 11:00:41 +0300

    database configuration

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
-        'NAME': BASE_DIR / config('DB_NAME', default='db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # Password validation
